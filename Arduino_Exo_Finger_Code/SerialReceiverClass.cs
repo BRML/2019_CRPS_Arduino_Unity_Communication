1363aff4537696ab4eb4a9d78c5598c88bf08555
Gustus <agn.gustus@gmail.com>
2019-05-27 14:06:27 +0200

    up serial communication incl MAX and MIN RANGE

--- Arduino_Exo_Finger_Code/SerialReceiverClass.cs
+++ Arduino_Exo_Finger_Code/SerialReceiverClass.cs
@@ -4,55 +4,91 @@ using UnityEngine;
 using System;
 using System.IO.Ports;
 using Ardunity;
+using UnityEngine.UI;
 
 public class SerialReceiverClass : MonoBehaviour
 {
+    public Slider max;
+    public Slider min;
     // Main contraints for hardware 
     float MIN_RANGE = 0;    // Minimum positon of the motors 
-    float MAX_RANGE = 0.5;  // Maximum positon of the motors 
+    float MAX_RANGE = 0.05f;  // Maximum positon of the motors 
     float MAX_FORCE = 10;   // Maximum force of the motors
 
     // Sensor variables 
-    public float fGSRValue = 0;             // [] Value of the GSR
-    public float fTemperature = 0;          // [C] Measured Temperature
-    public float fMAX_Assitance_Force = 0;  // [N] Maximum assistance force, set by the potentiometer    
-    public float[] Force = {0, 0, 0, 0};
+    public static float fGSRValue = 0;             // [] Value of the GSR
+    public static float fTemperature = 0;          // [C] Measured Temperature
+    public static float fMAX_Assitance_Force = 0;  // [N] Maximum assistance force, set by the potentiometer    
+    public static float[] Force = { 0, 0, 0, 0 };
 
     // Communication variables
-    float feedbackFreq = 4; // [1/s] Data is sent to the PC this many times per seconds
-    SerialPort serial = new SerialPort("COM3", 9600);  // Define COM Port and Baudrate
+    float feedbackFreq = 1f; // [1/s] Data is sent to the PC this many times per seconds
+    SerialPort stream = new SerialPort("COM3", 9600);  // Define COM Port and Baudrate
     int iReadtimeout = 100; // [ms]; sets the timeout for the readout
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    
+    public void WriteToArduino(string message)
+    {
+        stream.WriteLine(message);
+        stream.BaseStream.Flush();
+    }
 
     void setupArduino()
     {
-        // Send the main constraints
-        serial.WriteLine(feedbackFreq);
-        serial.WriteLine(MIN_RANGE);
-        serial.WriteLine(MAX_RANGE);
-        serial.WriteLine(MAX_FORCE);
+        WriteToArduino("I");
+        WriteToArduino(MIN_RANGE.ToString());
+        WriteToArduino("A");
+        WriteToArduino(MAX_RANGE.ToString());
+        //stream.WriteLine(MAX_FORCE.ToString());
     }
 
     // SETUP ROUTINE //
     void Start()
-   {
-        if (serial.IsOpen)
+    {
+        if (stream.IsOpen)
         {
-            serial.Close();   // If the serial port is still open, close it first
+            stream.Close();   // If the serial port is still open, close it first
         }
-        serial.Open();                      // (Re-)open the serial port
-        serial.ReadTimeout = iReadtimeout;  // sets the timeout for the readout
+        stream.Open();                      // (Re-)open the serial port
+        stream.ReadTimeout = iReadtimeout;  // sets the timeout for the readout
 
-       while (!serial.IsOpen) { }  // Wait for the serial port to open
+       
+        setupArduino();
+
     }
+
+
 
     //// MAIN LOOP ////
     void Update()
     {
-        if(serial.IsOpen)
+        //Debug.LogErrorFormat("running");
+        if (stream.IsOpen)
+        //    Debug.LogErrorFormat("stream is open");
+        //    Debug.LogErrorFormat(stream.ReadLine());
+
+        StartCoroutine
+            (
+                AsynchronousReadFromArduino
+                ((string s) => Debug.LogErrorFormat("s:" + s),     // Callback
+                   
+                    () => Debug.LogError("Error!"), // Error callback
+                    10000f                          // Timeout (milliseconds)
+                )
+            );
+        //setupArduino(); // test write the serial port
         {
             try
             {
-                string[] input = serial.ReadLine().Split(',');  // read the serial port and split the buffer 
+             
+
+
+                string[] input = stream.ReadLine().Split(',');  // read the serial port and split the buffer 
 
                 fTemperature = float.Parse(input[0]);           // Temperature
                 fGSRValue = float.Parse(input[1]);              // GSR
@@ -62,30 +98,87 @@ public class SerialReceiverClass : MonoBehaviour
                 Force[2] = float.Parse(input[5]);               // Force Sensor 3
                 Force[3] = float.Parse(input[6]);               // Force Sensor 4
 
-                serial.BaseStream.Flush();                      // clear the serial port buffer
+                stream.BaseStream.Flush();                      // clear the serial port buffer
             }
             catch (System.Exception) { }
-   
+
+        }
+        Print_Data2Console();
+        
+    }
+
+    public IEnumerator AsynchronousReadFromArduino(Action<string> callback, Action fail = null, float timeout = float.PositiveInfinity)
+    {
+        DateTime initialTime = DateTime.Now;
+        DateTime nowTime;
+        TimeSpan diff = default(TimeSpan);
+
+        string dataString = null;
+
+        do
+        {
+            try
+            {
+                dataString = stream.ReadLine();
+            }
+            catch (TimeoutException)
+            {
+                dataString = null;
             }
 
+            if (dataString != null)
+            {
+                callback(dataString);
+                yield break; // Terminates the Coroutine
+            }
+            else
+                yield return null; // Wait for next frame
+
+            nowTime = DateTime.Now;
+            diff = nowTime - initialTime;
+
+        } while (diff.Milliseconds < timeout);
+
+        if (fail != null)
+            fail();
+        yield return null;
+    }
+
+    public void changeminvalue()
+    {
+        
+        MIN_RANGE = min.value;
+        // Debug.LogErrorFormat("Minrange:" + MIN_RANGE);
+        WriteToArduino("I");
+        WriteToArduino(MIN_RANGE.ToString());
+        //setupArduino();
+    }
+
+    public void changemaxvalue()
+    {
+        MAX_RANGE = max.value;
+        // Debug.LogErrorFormat("maxrange:" + MAX_RANGE);
+        WriteToArduino("A");
+        WriteToArduino(MAX_RANGE.ToString());
+        //setupArduino();
     }
 
     // QUIT ROUTINE //
     void OnApplicationQuit()
     {
-        serial.Close(); // Close the serial port
+        stream.Close(); // Close the serial port
     }
 
     // FUNCTION : Prints the received data to Console
     void Print_Data2Console()
     {
-        Debug.LogErrorFormat(fMAX_Assitance_Force.ToString());
-        Debug.LogErrorFormat(fGSRValue.ToString());
-        Debug.LogErrorFormat(fTemperature.ToString());
-        Debug.LogErrorFormat(Force[0].ToString());
-        Debug.LogErrorFormat(Force[1].ToString());
-        Debug.LogErrorFormat(Force[2].ToString());
-        Debug.LogErrorFormat(Force[3].ToString());
+        //Debug.LogErrorFormat("slider"+fMAX_Assitance_Force.ToString());
+        //Debug.LogErrorFormat(fGSRValue.ToString());
+       // Debug.LogErrorFormat("Temperature"+fTemperature.ToString());
+       // Debug.LogErrorFormat(Force[0].ToString());
+       // Debug.LogErrorFormat(Force[1].ToString());
+       // Debug.LogErrorFormat(Force[2].ToString());
+       // Debug.LogErrorFormat(Force[3].ToString());
     }
 
 }
